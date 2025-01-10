2961df4ea0e838334dfb88a139eb8134562c35bd
= <=>
2025-01-10 13:18:28 -0600

    work in process for board class

--- Board.java
+++ Board.java
@@ -1,46 +1,105 @@
 import java.util.Arrays;
 
 public class Board {
-    char[] pieces = {'B','K','N','P','Q','R','b','k','n','b','q','r'}; //capital letters for white and lowercase letters for black
-    long[] bitboards; //array of size 12 with order of bitboards the same as order of pieces
-    
-    public Board(String fen){
+    char[] pieces = { 'B', 'K', 'N', 'P', 'Q', 'R', 'b', 'k', 'n', 'p', 'q', 'r' }; // capital letters for white and
+                                                                                    // lowercase letters for black
+                                                                                    // (needs to be in binarySearch
+                                                                                    // order)
+    long[] bitboards; // array of size 12 with order of bitboards the same as order of pieces
+
+    public Board(String fen) {
+
         initializeBitboards();
-        char[] fen_array = fen.toCharArray();
-        for(int i =0; i < fen_array.length;i++){
-           int bitboard_index = Arrays.binarySearch(pieces, (char) fen_array[i]);
-           if (bitboard_index > 0){
-            bitboards[bitboard_index] =  bitboards[bitboard_index] * 2 + 0b1;
+        char[] fen_array = fen.toCharArray(); //works as intended
+        
+        
+        for (int i = 0; i < fen_array.length; i++) {
             
-            for(int k = 0; k < pieces.length; k++){
-                if(k != i){
-                    bitboards[k] = bitboards[k] * 2 + 0b0;
-                }
+            int bitboard_index = Arrays.binarySearch(pieces, (char) fen_array[i]); // works as intended
+            
+            System.out.println("bitboard index for " + fen_array[i] + " is " + bitboard_index);
+            if (bitboard_index >= 0) {
+                System.out.println("bitboard index piece: " + pieces[bitboard_index]);
+                System.out.println("Bitboard for " + pieces[11]); //change 
+                System.out.println("Before: " + longtoBinary(bitboards[11]));
+                bitboards[bitboard_index] *= 0b10;
                 
-            }
-           }
-           else{
-            if (fen_array[i] != '/'){
-                int skips = (int) fen_array[i];
-                for(int j = 0; j < skips; j++){
-                    for(int k = 0; k < pieces.length; k++){
-                        bitboards[k] = bitboards[k] * 2 + 0b0;
+                bitboards[bitboard_index] += 0b1;
+                
+                
+                for (int k = 0; k < pieces.length; k++) {
+                    if (k != bitboard_index) {
+                        bitboards[k] = bitboards[k] * 0b10;
                     }
+
+                }
+                System.out.println("After: " + longtoBinary(bitboards[11]));
+                // System.out.println("After for loop for other bitboards: " + bitboards[bitboard_index]);
+            } else {
+                System.out.println("fen_array[i] is " + fen_array[i]);
+                if (fen_array[i] != '/') {
+                    int skips = Character.getNumericValue(fen_array[i]);
+                    // if(skips != 3){
+                    //     return;
+                    // }
+                    for (int j = 0; j < skips; j++) {
+                        for (int k = 0; k < pieces.length; k++) {
+                            // System.out.println( fen_array[k] + ": when blank bitboards before: " + bitboards[k]);
+                            bitboards[k] = bitboards[k] * 0b10;
+                            // System.out.println( fen_array[k] + ": when blank bitboards after : " + bitboards[k]);
+                        }
+                        System.out.println("bitboard for r after the " + (j+1) + " round of adding zero " + longtoBinary(bitboards[11]));
+
+                    }
+                    
                 }
             }
-           }
-           
+
         }
     }
-    public void initializeBitboards(){
+
+    public void initializeBitboards() {
         bitboards = new long[pieces.length];
-        for(int i = 0; i < pieces.length; i ++){
+        for (int i = 0; i < pieces.length; i++) {
             bitboards[i] = 0b0;
         }
     }
-    public void displayBitboards(){
-        for(int i = 0; i < pieces.length; i++){
-            System.out.println(bitboards[i]);
+
+    public void displayBitboards() {
+        for (int i = 0; i < pieces.length; i++) {
+            System.out.println("bitboard for " + pieces[i] + ": " + longtoBinary(bitboards[i]));
         }
+    }
+    public static String longtoBinary(long l){
+        int n = 0;
+        String binaryString = "";
+        boolean running = true;
+        int i = 0;
+        while (running){
+            
+            if (l < (long) Math.pow(2, i)){
+                n = i-1;
+                // System.out.println("n = " + n);
+                running = false;
+            }
+            i++;
+            
+        }
+        // System.out.println("n = " + n);
+
+        for(int j = n; j >=0; j--){
+            if (l >= Math.pow(2, j)){
+                l = l - (long) Math.pow(2, j);
+                binaryString = binaryString + "1";
+            }
+            else{
+                binaryString = binaryString +"0";
+
+            }
+        }
+        if (n == -1){
+            binaryString = "0";
+        }
+        return binaryString;
     }
 }
--- Main.java
+++ Main.java
@@ -3,8 +3,20 @@ public class Main {
         Board b = new Board("r3r1k1/pp3nPp/1b1p1B2/1q1P1N2/8/P4Q2/1P3PK1/R6R");
         System.out.println("-----------------------");
         b.displayBitboards();
-        // long bitboard = 0b000000001000;
+        
+        // long bitboard = 0b0001000;   
         // System.out.println(bitboard);
-        // System.out.println(bitboard + 0b010);
+        // System.out.println(bitboard * 0b10);
+
+        // System.out.println(Board.longtoBinary(9837l));
+        // long l = 0b0;
+        // l = l * 0b10;
+        // l = l + 0b1;
+
+        // l = l * 0b10;
+        // l = l * 0b10;
+        // l = l * 0b10;
+        // System.out.println(l);
+
     }
 }
